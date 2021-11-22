3c99e880fa5517811dd28adb92b68c7785906c3e
Matthew Dillon <mddillon47@gmail.com>
2021-11-22 12:57:36 -0800

    heroku

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,8 @@ const pg = require("pg");
 
 const db = new pg.Client({
   Host: "ec2-23-23-133-10.compute-1.amazonaws.com",
-    Database: "postgres://jwidpezbkdtmet:c0bea0036c84f7b6b89e3d0beb9a15352d9abd40283b48df4722d428f52e9df2@ec2-23-23-133-10.compute-1.amazonaws.com:5432/d25kdq8jhkd5dd"
+    Database: "d25kdq8jhkd5dd",
+  Port: 5432
 });
 
 db.connect();
