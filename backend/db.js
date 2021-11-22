26311353cc0d403bc0f60abb27a59c0a2f421f91
Matthew Dillon <mddillon47@gmail.com>
2021-11-21 17:57:13 -0800

    db update

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,9 @@
 const pg = require("pg");
 
-const db = new pg.Client("postgresql:///colorings_db");
+
+const db = new pg.Client(
+  "postgres://jwidpezbkdtmet:c0bea0036c84f7b6b89e3d0beb9a15352d9abd40283b48df4722d428f52e9df2@ec2-23-23-133-10.compute-1.amazonaws.com:5432/d25kdq8jhkd5dd"
+);
 
 db.connect();
 
