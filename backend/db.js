60e997aa8326394b613392d1da74cfedd1c11267
Matthew Dillon <mddillon47@gmail.com>
2021-11-22 14:47:08 -0800

    db

--- backend/db.js
+++ backend/db.js
@@ -1,13 +1,8 @@
 const pg = require("pg");
 
-const db = new pg.Client({
-  user: "jwidpezbkdtmet",
-  password: "c0bea0036c84f7b6b89e3d0beb9a15352d9abd40283b48df4722d428f52e9df2",
-  database: "d25kdq8jhkd5dd",
-  port: 5432,
-  host: "ec2-23-23-133-10.compute-1.amazonaws.com",
-  ssl: false,
-});
+const db = new pg.Client(
+  "postgres://jwidpezbkdtmet:c0bea0036c84f7b6b89e3d0beb9a15352d9abd40283b48df4722d428f52e9df2@ec2-23-23-133-10.compute-1.amazonaws.com:5432/d25kdq8jhkd5dd"
+);
 
 db.connect();
 
