cb97647d09e45dc5770567ff07156cb1b48386fe
Matthew Dillon <mddillon47@gmail.com>
2021-11-21 22:18:01 -0800

    src

---
+++ public/src/actions/actions.js
@@ -0,0 +1,157 @@
+import axios from 'axios';
+import { GET_USER } from './types';
+
+const BASE_URL = 'http://localhost:9000';
+
+function getUser(username, id) {
+	return {
+		type     : GET_USER,
+		username, id
+	};
+}
+
+export function getUserFromAPI(data) {
+	return async function(dispatch) {
+		const response = await axios({
+			method : 'post',
+			url    : `${BASE_URL}/users/login`,
+			data   : {
+				username : `${data.username}`,
+				password : `${data.password}`
+			}
+		});
+		console.log(response)
+		return dispatch(getUser(response.data.username, response.data.id));
+	};
+}
+
+export function registerUser(data) {
+	return async function(dispatch) {
+		const response = await axios({
+			method : 'post',
+			url    : `${BASE_URL}/users`,
+			data   : {
+				username : data.username,
+				password : data.password
+			}
+		});
+		return dispatch(getUser(response.data.username, response.data.id));
+	};
+}
+
+export async function postColoringsToAPI(name, image, userId) {
+		const response = await axios({
+			method : 'post',
+			url    : `${BASE_URL}/colorings`,
+			data   : {
+				name : name,
+				image : image, 
+				user_id: userId
+			}
+		});
+		return response.data
+	
+}
+
+export async function getUserColoringsFromAPI(user_id){
+	const response = await axios({
+		method: 'get', 
+		url : `${BASE_URL}/colorings/${user_id}`,
+
+	})
+	return response.data
+}
+// function getPost(post) {
+//     return {
+//         // type: GET_POST,
+//         post
+//     }
+// }
+// export function getPostFromAPI(id){
+//     return async function (dispatch) {
+//         const response = await axios.get(`${API_URL}/${id}`)
+//         console.log(response.data)
+//         return dispatch(getPost(response.data))
+//     }
+// }
+// function addPost(values) {
+//     const id = values.id
+//     return {
+//         // type: CREATE_POST,
+//         values,
+//         id
+//     }
+// }
+// export function createPostInAPI({title, description, body}){
+//     console.log(title)
+//     return async function(dispatch){
+//         const response = await axios.post(`${API_URL}`, {
+//             title,
+//             description,
+//             body
+//         })
+
+//         return dispatch(addPost(response.data))
+//     }
+// }
+// function deletePost(id) {
+//     return {
+//         // type: DELETE_POST,
+//         id
+//     }
+// }
+// export function deletePostFromAPI(id) {
+//     return async function(dispatch){
+//         await axios.delete(`${API_URL}/${id}`)
+//         return dispatch(deletePost(id))
+//     }
+// }
+
+// function editPost(values){
+//     const id = values.id
+//     return {
+//         // type: EDIT_POST,
+//         values,
+//         id
+//     }
+// }
+// export function editPostInAPI({id, title, description, body}) {
+
+//     return async function(dispatch){
+//         const response = await axios.put(`${API_URL}/${id}`, {
+//             title,
+//             description,
+//             body
+//         })
+
+//         return dispatch(editPost(response.data))
+//     }
+// }
+
+// function addComment(id, comment){
+//     return {
+//         // type: CREATE_COMMENT,
+//         id,
+//         comment
+//     }
+// }
+// export function sendCommentToAPI(text, postId){
+//     const id = postId
+//     return async function(dispatch){
+//         const response = await axios.post(`${API_URL}/${postId}/comments`, {text})
+//         return dispatch(addComment(id, response.data))
+//     }
+// }
+// function deleteComment(id, commentId){
+//     return {
+//         // type: DELETE_COMMENT,
+//         id,
+//         commentId
+//     }
+// }
+// export function removeCommentFromAPI(id, commentId){
+//     return async function(dispatch){
+//         await axios.delete(`${API_URL}/${id}/comments/${commentId}`)
+//         return dispatch(deleteComment(id, commentId))
+//     }
+// }
---
+++ public/src/helpers/getBooks.js
@@ -0,0 +1,62 @@
+const RandomText = [
+  "An elephant taking a shower",
+  "Oh, the Places You'll Go!",
+  "Mickey and Pluto at the beach",
+  "The octopus from Finding Dory",
+  "Oh, the Places You'll Go!",
+  "Oh, the Places You'll Go!",
+];
+
+const PeppaText = [
+  "",
+  "It was a beautiful, sunny day at the Peppa pig house.",
+  "Peppa: Good morning George. George: Oink Oink. Peppa: I can't wait to go outside and play!",
+  "Mommy pig has just finished making breakfast. Mommy pig: Peppa! George! Time for breakfast!",
+  "While daddy pig reads the morning newspaper. Daddy pig: Mmm, something smells good.",
+  "After breakfast, it was finally time to play. Mommy pig: Uh no, it's raining...",
+  "Good thing Peppa and George LOVE jumping up and down in muddy puddles!",
+  "Oh my goodness! Wow! So much mud!. Splish, Splash, Splosh, Splish, Splash. The End.",
+];
+// const className = ((i + 1) % 2 ) === 0 ? "left" : "right"
+
+export function getBooks(bookName) {
+  if (bookName === "Random") {
+    const Randomsvgs = require.context(
+      `../assets/images/Random`,
+      true,
+      /\.svg$/
+    );
+    const RandomArr = Randomsvgs.keys().map((path) => ({
+      path,
+      file: Randomsvgs(path),
+    }));
+    const Random = [];
+
+    for (let i = 0; i < RandomArr.length; i++) {
+      const className = (i + 1) % 2 !== 0 ? "left" : "right";
+      Random.push({
+        path: RandomArr[i].path,
+        text: RandomText[i],
+        className: className,
+      });
+    }
+    return Random;
+  } else if (bookName === "Peppa") {
+    const Peppasvgs = require.context(`../assets/images/Peppa`, true, /\.svg$/);
+    const PeppaArr = Peppasvgs.keys().map((path) => ({
+      path,
+      file: Peppasvgs(path),
+    }));
+    const Peppa = [];
+
+    for (let i = 0; i < PeppaArr.length; i++) {
+      const className = (i + 1) % 2 !== 0 ? "left" : "right";
+      Peppa.push({
+        path: PeppaArr[i].path,
+        text: PeppaText[i],
+        className: className,
+      });
+    }
+    return Peppa;
+  }
+}
