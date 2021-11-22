d295c518c62ba0147aa0213e01a4621f96ebaf54
Matthew Dillon <mddillon47@gmail.com>
2021-11-21 21:06:27 -0800

    heroku

--- backend/app.js
+++ backend/app.js
@@ -14,17 +14,17 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 
 app.use(logger("dev"));
-app.use(express.json({limit:"50mb"}));
-app.use(express.urlencoded({ extended: false, limit:"50mb" }));
-
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: false, limit: "50mb" }));
 
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "frontend/build")));
 app.use(cors());
 app.use("/users", uRouter);
 app.use("/colorings", cRouter);
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+app.get("*", function (req, res) {
+  const index = path.join(__dirname, "build", "index.html");
+  res.sendFile(index);
 });
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
