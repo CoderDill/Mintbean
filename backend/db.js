e1eded875b32e4a1a9625a201efcb0dab0e9d688
Matthew Dillon <mddillon47@gmail.com>
2021-11-22 12:48:43 -0800

    heroku

--- backend/db.js
+++ backend/db.js
@@ -1,12 +1,8 @@
 const pg = require("pg");
 
-const db = new pg.Client({
-  database:
-    "postgres://jwidpezbkdtmet:c0bea0036c84f7b6b89e3d0beb9a15352d9abd40283b48df4722d428f52e9df2@ec2-23-23-133-10.compute-1.amazonaws.com:5432/d25kdq8jhkd5dd",
-  ssl: "true",
-  rejectUnauthorized: false,
-  public: "store-client-nestroia1.c9users.io",
-});
+const db = new pg.Client(
+  "postgres://jwidpezbkdtmet:c0bea0036c84f7b6b89e3d0beb9a15352d9abd40283b48df4722d428f52e9df2@ec2-23-23-133-10.compute-1.amazonaws.com:5432/d25kdq8jhkd5dd"
+);
 
 db.connect();
 
