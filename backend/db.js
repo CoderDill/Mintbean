05a6d0913e042fe9075c38eb896ecae03ccd8a2d
Matthew Dillon <mddillon47@gmail.com>
2021-11-22 16:33:39 -0800

    db

--- backend/db.js
+++ backend/db.js
@@ -1,13 +1,11 @@
 const pg = require("pg");
 
-const db = new pg.Client("postgresql:///colorings_db");
+const db = new pg.Client({
+  host: "ec2-23-23-133-10.compute-1.amazonaws.com",
+  database: process.env.DATABASE_URL,
+  ssl: true,
+});
 
 db.connect();
 
 module.exports = db;
-
-// {
-//   host: "ec2-23-23-133-10.compute-1.amazonaws.com",
-//   database: process.env.DATABASE_URL,
-//   ssl: true,
-// }
