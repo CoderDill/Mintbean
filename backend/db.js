50c2402a3632115a880a174db1e9e161b6e419c6
Matthew Dillon <mddillon47@gmail.com>
2021-11-22 19:04:39 -0800

    db

--- backend/db.js
+++ backend/db.js
@@ -4,6 +4,8 @@ const db = new pg.Client({
   host: "ec2-23-23-133-10.compute-1.amazonaws.com",
   database: process.env.DATABASE_URL,
   ssl: true,
+  port: 5432,
+  user: "jwidpezbkdtmet",
 });
 
 db.connect();
