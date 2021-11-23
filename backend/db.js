2b300e73969299827be6b0e290fe74266c2a8b6c
Matthew Dillon <mddillon47@gmail.com>
2021-11-22 19:06:02 -0800

    db

--- backend/db.js
+++ backend/db.js
@@ -3,7 +3,7 @@ const pg = require("pg");
 const db = new pg.Client({
   host: "ec2-23-23-133-10.compute-1.amazonaws.com",
   database: process.env.DATABASE_URL,
-  ssl: true,
+  ssl: { rejectUnauthorized: false },
   port: 5432,
   user: "jwidpezbkdtmet",
 });
