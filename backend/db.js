7bb0ad79b5b52bf9fa8fbb1e2c2874fe5f15ec0f
Matthew Dillon <mddillon47@gmail.com>
2021-11-22 16:31:04 -0800

    db fix from heroku

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,13 @@
 const pg = require("pg");
 
-const db = new pg.Client({
-  host: "ec2-23-23-133-10.compute-1.amazonaws.com",
-  database: process.env.DATABASE_URL,
-  ssl: true,
-});
+const db = new pg.Client("postgresql:///colorings_db");
 
 db.connect();
 
 module.exports = db;
+
+// {
+//   host: "ec2-23-23-133-10.compute-1.amazonaws.com",
+//   database: process.env.DATABASE_URL,
+//   ssl: true,
+// }
